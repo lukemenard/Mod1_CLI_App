2644be306fbf8d005804d9018b4c17a1aab58c15
lukemenard <lukemenard@gmail.com>
2019-07-30 12:37:18 -0600

    Working on CLI

---
+++ Models/CLI/main_menu_methods.rb
@@ -0,0 +1,14 @@
+
+class MainMenuMethod
+  def self.list_all_trees
+    puts "Hello"
+  end
+
+  def self.list_trees_by_characteristics
+    puts "butt"
+  end
+
+  def self.main_menu_help
+    puts "poop"
+  end
+end
---
+++ Models/CLI/question_1.rb
@@ -0,0 +1,14 @@
+class Question1
+
+  def self.question_one
+    puts "1. My tree is coniferous."
+    puts "2. My tree is deciduous."
+    @@response = gets.chomp.to_i
+
+    if @@response
+    end
+
+
+  end
+
+end
