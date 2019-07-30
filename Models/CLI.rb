5eeab41a95273485eb5d8988cbf7e2189a2ebc56
lukemenard <lukemenard@gmail.com>
2019-07-30 15:41:16 -0600

    revamping questions/CLI structures

--- Models/CLI.rb
+++ Models/CLI.rb
@@ -11,37 +11,88 @@ class Application
 
   def self.get_username
     system "clear"
-    puts "Hello! Welcome to the Colorado Tree ID App! What's your name?".white.on_green
+    puts "Hello! Welcome to the Colorado Tree ID App!".white.on_green
+    puts "What's your name?".white.on_green
     @@user = gets.chomp
   end
   self.get_username
-
-  system "clear"
 
 
 # ____________MAIN_MENU___________________________
 
   def self.main_menu
-    puts "Hello, #{@@user}! What would you like to do today?"
+    system "clear"
+    puts "Hello, #{@@user}! What would you like to do?"
     puts "    1. Identify a tree."
     puts "    2. See a list of all available trees."
     puts "    3. See a list of all available trees by characteristic."
     puts "    4. Learn how to use this app."
     @@response = gets.chomp.to_i
 
-    if @@response == 1
+    case @@response
+    when 1
       Question.question_one
-    elsif @@response == 2
+    when 2
       Help.list_all_trees
-    elsif @@response == 3
+    when 3
       Help.list_trees_by_characteristics
-    elsif @@response == 4
+    when 4
       Help.main_menu_help
     else
-      puts "Please choose a correct choice: 1 - 4"
+      puts "Please enter a number between 1 and 4"
       # self.main_menu
     end
   end
   self.main_menu
 
+
+# ____________Question_1___________________________
+
+  def self.question_one
+    case @@response
+    when 1
+      type = "coniferous"
+      Question.question_two_conifer
+    when 2
+      type = "deciduous"
+      Question.question_two_deciduous
+    when 3
+      MainMenuMethod.list_trees_by_characteristics
+    when 4
+      Application.main_menu
+    else
+      puts "Please enter a number between 1 and 4"
+      self.question_one
+    end
+  end
+  Application.question_one
+
+
+  # ____________Question_2___________________________
+
+  def self.question_two_conifer
+    case @@response
+    when 1
+      needles = "Clusters of 2-3"
+      Question.question_three_conifer
+    when 2
+      needles = "Clusters of 5"
+      Question.question_three_conifer
+    when 3
+      needles = "Flat"
+      Question.question_three_conifer
+    when 4
+      needles = "Sharp"
+      Question.question_three_conifer
+    when 5
+      needles = "Scaled"
+      Question.question_three_conifer
+    when 6
+      Help.list_all_trees
+    else
+      puts "Please enter a number between 1 and 4"
+      self.question_two_conifer
+    end
+  end
+  Application.question_two_conifer
 end
--- Models/questions.rb
+++ Models/questions.rb
@@ -1,42 +1,79 @@
 class Question
 
+
+# ____________Question_1___________________________
+
   def self.question_one
     system "clear"
+
+    puts "Conifers are trees with cones and needle-like or scale-like leaves that are typically evergreen (retain their leaves throughout the year)."
+    puts "Pines and firs are conifers, for example."
+    puts "Deciduous trees lose their leaves annually."
+    puts "Maples and oaks are deciduous trees."
     puts "Is your tree coniferous or deciduous?"
     puts "    1. My tree is coniferous."
     puts "    2. My tree is deciduous."
     puts "    3. Get help on this question."
     puts "    4. Return to Main Menu."
     @@response = gets.chomp.to_i
-
-    if @@response == 1
-      Question.question_two_conifer
-    elsif @@response == 2
-      Question.question_two_deciduous
-    elsif @@response == 3
-      MainMenuMethod.list_trees_by_characteristics
-    elsif @@response == 4
-      Application.main_menu
-    else
-      puts "Please enter a number between 1 and 4"
-      self.main_menu
-    end
   end
 
-
+# ____________Question_2_Conifer___________________________
 
   def self.question_two_conifer
-
+    system "clear"
+    puts "Your tree is a conifer! There are XX available conifers."
+    puts "The way a conifer's needles are arranged can help you identify the species."
+    puts "A conifer can have individual or clustered needles, called fasciles."
+    puts "Needles can also be flat or sharp."
+    puts "What needle arrangement does your tree have?"
+    puts "    1. Needles are in clusters of 2-3."
+    puts "    2. Needles are in clusters of 5."
+    puts "    3. Needles are flat."
+    puts "    4. Needles are sharp."
+    puts "    5. Needles are scaled."
+    puts "    6. See a list of x remaining trees."
+    @@response = gets.chomp.to_i
   end
 
+# ____________Question_3_Conifer___________________________
 
+def self.question_three_conifer
+  system "clear"
+  puts "Your tree is a conifer! There are XX available conifers."
+  puts "The way a conifer's needles are arranged can help you identify the species."
+  puts "A conifer can have individual or clustered needles, called fasciles."
+  puts "Needles can also be flat or sharp."
+  puts "What needle arrangement does your tree have?"
+  puts "    1. Needles are in clusters of 2-3."
+  puts "    2. Needles are in clusters of 5."
+  puts "    3. Needles are flat."
+  puts "    4. Needles are sharp."
+  puts "    5. Needles are scaled."
+  puts "    6. See a list of x remaining trees."
+  @@response = gets.chomp.to_i
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ____________Question_2_Deciduous___________________________
 
   def self.question_two_deciduous
-
+    puts "Your tree is deciduous! There XX available deciduous trees."
+    puts "The way a tree's leaves are arranged can help you identify the species."
   end
-
-
-
 
 
 end
