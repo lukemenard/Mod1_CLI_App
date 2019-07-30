892c41330aad6e27cc72053b8081244dc4413639
lukemenard <lukemenard@gmail.com>
2019-07-30 09:55:38 -0600

    Commiting CLI file changes

--- Models/CLI.rb
+++ Models/CLI.rb
@@ -4,8 +4,10 @@ class Application
   def self.main_menu
     puts "Hello! Welcome to the Colorado Tree ID App! What's your name?"
     @@user = gets.chomp
-    puts "Hello #{@@user}! Here's "
-    @@response = gets.chomp"
+    puts "Hello #{@@user}! "
+    @@response = gets.chomp
+  end
 
+  self.main_menu
 
-    
+end
